0773a26dfba8bcba9dc97788e0180aa1faec43d8
mannansMergeStackSpace <abdul.mannan@mergestack.com>
2023-11-29 19:08:16 +0500

    minor fixes

--- index.ts
+++ index.ts
@@ -8,8 +8,6 @@ const cors = require("cors");
 const corsOptions = {
   origin: "*", // Replace with your allowed origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Include cookies
-  optionsSuccessStatus: 204, // Respond with a 204 status for preflight requests
 };
 
 dotenv.config();
